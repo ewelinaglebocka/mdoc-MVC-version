1e8a46f2179701c99fa1cc84db00fb5776604b5a
Ewelina Głębocka <ewelina.glebocka@mardom.com.pl>
2022-08-29 15:44:49 +0200

    Executive dok repository added

---
+++ mdoc/Repositories/ExecutiveDocumentationRepository.cs
@@ -0,0 +1,19 @@
+﻿using mdoc.DataBase.Models.Dokumentacja_wykonawcza;
+
+namespace mdoc.Repositories
+{
+    public class ExecutiveDocumentationRepository : IExecutiveDocumentationRepository
+    {
+        private readonly mdocContext _context;
+        public ExecutiveDocumentationRepository(mdocContext context)
+        {
+            _context = context;
+        }
+        public List<dokumenty> GetAll()
+        {
+            var documents = _context.documents;
+
+            return documents.ToList();
+        }
+    }
+}
---
+++ mdoc/Repositories/IExecutiveDocumentationRepository.cs
@@ -0,0 +1,9 @@
+﻿using mdoc.DataBase.Models.Dokumentacja_wykonawcza;
+
+namespace mdoc.Repositories
+{
+    public interface IExecutiveDocumentationRepository
+    {
+        public List<dokumenty> GetAll();
+    }
+}
